8abb6901032d986fee2e6b9d65ca60412628af3d
deivid22srk <deivid22srk@users.noreply.github.com>
2026-06-13 11:55:53 -0300

    Fix Kotlin compilation error: use collectAsState for StateFlow

--- app/src/main/java/com/deivid22srk/skvodsapp/MainActivity.kt
+++ app/src/main/java/com/deivid22srk/skvodsapp/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -96,7 +97,7 @@ fun MainScreen() {
 
 @Composable
 fun HomeScreenContent(viewModel: HomeViewModel = viewModel()) {
-    val uiState by viewModel.uiState
+    val uiState by viewModel.uiState.collectAsState()
 
     when (val state = uiState) {
         is UiState.Loading -> {
